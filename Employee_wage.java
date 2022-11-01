739ee5a8ee427dea1ec9b3757ea6e7bf8f9bec72
sagar <sagarbehera156@gmail.com>
2022-11-01 21:30:04 +0530

    Add: Calculate Daily Employee Wage

--- Employee_wage.java
+++ Employee_wage.java
@@ -2,16 +2,28 @@ package EmpWage;
 
 public class Employee_wage {
 	 int Fulltime =1;
+	 int parttime=2;
 	 int wageperhr=20;
-      double empcheck =(int)(Math.random()*2);
+      double empcheck =(int)(Math.random()*3);
       int emphrs;
       int empwage;
    public void Check_Emp_Is_PresentorAbsent() {
 	   if (empcheck==Fulltime) {
+	    	  System.out.println("Employee is present");
+	      }
+	    	  else {
+	    		  System.out.println("Employee is absent");
+	    	  }
+   }
+   public void Daily_Emp_Wage() {
+	   if (empcheck==Fulltime) {
 		 	  emphrs=8;
+		 	 System.out.println("Employee Hour:"+emphrs);
 		   }
+	 
 		 	  else {
 		 		  emphrs=0;
+		 		 System.out.println("Employee Hour:"+emphrs);
 		 	  }
 	   empwage = emphrs*wageperhr;
 	   System.out.println("Daily Employee Wages:"+empwage);
@@ -21,6 +33,7 @@ public class Employee_wage {
 System.out.println("Welcome to Employee Wage Computation Program");
 Employee_wage emp=new Employee_wage();
 emp.Check_Emp_Is_PresentorAbsent() ;
+emp.Daily_Emp_Wage();
 	}
 }
 
