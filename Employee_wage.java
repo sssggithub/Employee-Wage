8ad4b0f77ab45a4c286a165c77b785b5e296e5ca
sagar <sagarbehera156@gmail.com>
2022-11-01 18:17:06 +0530

    Displaying

---
+++ Employee_wage.java
@@ -0,0 +1,10 @@
+package EmpWage;
+
+public class Employee_wage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Welcome to Employee Wage Computation Program");
+	}
+
+}
