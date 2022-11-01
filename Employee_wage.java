11cf7abc3c0e69493a929b539dbf58070d0ca79f
sagar <sagarbehera156@gmail.com>
2022-11-01 18:34:02 +0530

    Add: Check Employee is Present or Absent

--- Employee_wage.java
+++ Employee_wage.java
@@ -1,10 +1,22 @@
 package EmpWage;
 
 public class Employee_wage {
-
+	 static int Fulltime =1;
+     static double empcheck =Math.floor(Math.random()*10)%2;
+   public void Check_Emp_Is_PresentorApsent() {
+	   if (empcheck==Fulltime) {
+		 	  System.out.println("Employee is present");
+		   }
+		 	  else {
+		 		  System.out.println("Employee is apsent");
+		 	  }
+   }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Welcome to Employee Wage Computation Program");
+Employee_wage emp=new Employee_wage();
+emp.Check_Emp_Is_PresentorApsent() ;
 	}
-
 }
+
+
